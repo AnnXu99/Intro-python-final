b456c6c9d3dc02bfd14ab571c3f632f8023a3741
annxu99 <annxuleheng@gmail.com>
2020-06-11 21:42:58 -0700

    x sin area

---
+++ AnnISinArea.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import csv
+
+x_all = []
+y_all = []
+x = []
+y = []
+
+data_x = open('C:/Users/bucol/IdeaProjects/Intro-python-final1/dataX2.csv')
+data_y = open('C:/Users/bucol/IdeaProjects/Intro-python-final1/dataY2.csv')
+
+x_reader = csv.reader(data_x)
+count = 0;
+for row in x_reader:
+    if ('X values of sin' in row ):
+        continue
+    for field in row:
+        x_all.append(float(field))
+        if (count < 251):
+            x.append(float(field))
+            count +=1
+
+y_reader = csv.reader(data_y)
+count = 0;
+for row in y_reader:
+    if ('Y values of sin' in row ):
+        continue
+    for field in row:
+        y_all.append(float(field))
+        if (count <251):
+            y.append(float(field))
+            count += 1
+
+area = 0.0
+delta = x[1]-x[0]
+for i in range(250):
+    area += y[i]*delta
+
+plt.plot(x_all, y_all)
+plt.fill_between(x, y)
+plt.text(0.25, -5, "area = " + str(area))
+plt.show()
